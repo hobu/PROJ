ce1b7717172cae6c0af0ac022f4c7a73280d931c
Howard Butler <hobu.inc@gmail.com>
2023-01-02 16:23:55 -0600

    quote CONDA_PREFIX

--- docs/pre_install.sh
+++ docs/pre_install.sh
@@ -3,5 +3,5 @@
 
 mkdir build
 cd build
-cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF -DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX ..
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF -DCMAKE_INSTALL_PREFIX="$CONDA_PREFIX" ..
 ninja install
