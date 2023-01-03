821d52996548406b98ec9fa9f2696a31a94d605d
Howard Butler <hobu.inc@gmail.com>
2023-01-03 11:02:48 -0600

    explicit install prefix

--- docs/pre_install.sh
+++ docs/pre_install.sh
@@ -5,5 +5,5 @@ mkdir build
 cd build
 echo "CONDA_PREFIX: ${CONDA_PREFIX}"
 
-cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF -DCMAKE_INSTALL_PREFIX="$CONDA_PREFIX" ..
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF -DCMAKE_INSTALL_PREFIX="/home/docs/checkouts/readthedocs.org/user_builds/proj-local/conda/$READTHEDOCS_VERSION" ..
 ninja install
