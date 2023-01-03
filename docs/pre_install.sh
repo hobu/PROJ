b9ce027f3e93644bf6206f9d5f1660ddd721bc8e
Howard Butler <hobu.inc@gmail.com>
2023-01-03 12:31:28 -0600

    conda activate

--- docs/pre_install.sh
+++ docs/pre_install.sh
@@ -5,5 +5,10 @@ mkdir build
 cd build
 echo "CONDA_PREFIX: ${CONDA_PREFIX}"
 
-cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF -DCMAKE_INSTALL_PREFIX="/home/docs/checkouts/readthedocs.org/user_builds/proj-local/conda/$READTHEDOCS_VERSION" ..
+CONDA_PREFIX="/home/docs/checkouts/readthedocs.org/user_builds/proj-local/conda/$READTHEDOCS_VERSION"
+
+$CONDA_PREFIX/bin/activate
+
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF -DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX -DCMAKE_PREFIX_PATH=$CONDA_PREFIX ..
+
 ninja install
