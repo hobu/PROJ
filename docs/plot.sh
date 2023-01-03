c283ac19eb91012c032804e07a2d5dee7cadd0ed
Howard Butler <hobu.inc@gmail.com>
2023-01-03 14:04:13 -0600

    no pushd/popd

--- docs/plot.sh
+++ docs/plot.sh
@@ -6,9 +6,9 @@ CONDA_PREFIX="/home/docs/checkouts/readthedocs.org/user_builds/proj-local/conda/
 export PATH="$CONDA_PREFIX/bin:$PATH"
 export PROJ_DATA="$CONDA_PREFIX/share/proj"
 
-pushd docs/plot
+cd docs/plot
 python plot.py plotdefs.json images
-popd
+cd ../..
 
 mkdir -p docs/source/operations/projections/images/
 cp -r docs/plot/images/* docs/source/operations/projections/images/
