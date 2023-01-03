491630acb4b2a8160191b594df472aebcd929fc0
Howard Butler <hobu.inc@gmail.com>
2023-01-03 13:07:19 -0600

    rpath-link

--- docs/pre_install.sh
+++ docs/pre_install.sh
@@ -3,12 +3,13 @@
 
 mkdir build
 cd build
-echo "CONDA_PREFIX: ${CONDA_PREFIX}"
 
 CONDA_PREFIX="/home/docs/checkouts/readthedocs.org/user_builds/proj-local/conda/$READTHEDOCS_VERSION"
 
+echo "CONDA_PREFIX: $CONDA_PREFIX"
+
 $CONDA_PREFIX/bin/activate
 
-cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF -DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX -DCMAKE_PREFIX_PATH=$CONDA_PREFIX ..
+LDFLAGS="-Wl,-rpath-link,$CONDA_PREFIX/lib" cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF -DCMAKE_INSTALL_PREFIX="$CONDA_PREFIX" -DCMAKE_PREFIX_PATH="$CONDA_PREFIX" ..
 
 ninja install
